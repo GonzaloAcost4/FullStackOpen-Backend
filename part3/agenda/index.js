f9d6176434649c24c48ef7e1196597e606340e3a
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2026-04-21 00:00:30 -0300

    agenda 2.6 done with unique  number control

--- part3/agenda/index.js
+++ part3/agenda/index.js
@@ -39,6 +39,63 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
+app.get ('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person){
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => { 
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person){
+    const index = persons.indexOf(person) // obtiene el índice del elemento seleccionado del array persons
+    persons.splice(index, 1) // elimina el elemento seleccionado del array persons 
+    response.status(204).end() // devuelve una respuesta sin contenido
+  } else {
+    response.status(404).end() // devuelve una respuesta de error si el elemento no se encuentra
+  }
+})
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000)
+  return id
+} 
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body 
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  } else if (persons.find(person => person.number === body.number)) { 
+    return response.status(400).json({ 
+      error: 'number must be unique' 
+    })
+  }
+
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons.push(person) // agrega el nuevo objeto person al array persons
+
+  response.json(person)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
